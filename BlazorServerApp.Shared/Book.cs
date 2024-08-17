990434452ef9e4939b2d7d1ef92fdf2268d42c2a
guy <guyg7@mail.afeka.ac.il>
2024-08-17 19:31:37 +0300

    added Author entity and a query that fetches books join authors on id

--- BlazorServerApp.Shared/Book.cs
+++ BlazorServerApp.Shared/Book.cs
@@ -4,10 +4,11 @@
     {
         public long Id { get; set; }
         public string Name { get; set; }
-        public string Author { get; set; }
+        public long AuthorId { get; set; }
         public int? Quantity { get; set; }
         public int Price { get; set; }
         public bool Available { get; set; }
+        public Author Author { get; set; }
     }
 
 }
