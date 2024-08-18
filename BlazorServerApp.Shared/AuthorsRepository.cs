6fde3a6e36587bcc7f30fdbb8278482bb73ab0b7
guy <guyg7@mail.afeka.ac.il>
2024-08-18 12:23:51 +0300

    add authors repository

---
+++ BlazorServerApp.Shared/AuthorsRepository.cs
@@ -0,0 +1,27 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace BlazorServerApp.Shared
+{
+    public class AuthorsRepository
+    {
+        private readonly string _connectionString;
+        public AuthorsRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public List<Author> GetAuthors()
+        {
+            using (var connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                return connection.Query<Author>("SELECT * FROM Authors").ToList();
+            }
+        }
+    }
+}
