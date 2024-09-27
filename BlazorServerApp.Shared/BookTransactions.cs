403750a702838f76d568d62a0c59caead968836e
guy <guyg7@mail.afeka.ac.il>
2024-09-27 16:22:56 +0300

    new BookTransaction class

---
+++ BlazorServerApp.Shared/BookTransactions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Shared
+{
+    public class BookTransaction
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string TransactionType { get; set; } = "LOAN"; // 'Loan' or 'Sale'
+        public int Quantity { get; set; } 
+        public string LoanerDetails { get; set; } // JSON field for loaner details
+        public DateTime TransactionDate { get; set; } = DateTime.Now;
+    }
+
+}
